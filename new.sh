8c6cef1438632f3a0576ead19c6229ec4f5de192
jx <jx@win>
2021-06-22 15:19:02 +0800

    fix new.sh

--- new.sh
+++ new.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+# usage
 if [ $# != 1 ]; then
   echo "Usage:"
   echo "  $0 article_name"
   exit 1
 fi
 
+# env
 DST=./content
 TITLE=$1
 DATE=$(date +%Y-%m-%d)
 FILE=$DATE-$TITLE.md
 
 
-# new file
+# create file
 if [ -f $FILE ]; then
   echo "[ERROR] Found $FILE already!"
   exit 1
@@ -35,9 +37,9 @@ weight: 10
 
 EOF
 
-cd -
-
+# deploy file
 mv $FILE $DST
 
+# log
 echo "[INFO] Create new file successfully."
 echo "[INFO] vim $DST/$FILE"
